9dc5f5b6923ce89da49021eb9a6935266f49be21
Guilherme Costa <guizeracostasilva@outlook.com>
2024-05-05 12:32:19 -0300

    Comparando se o chute foi correto ou incorreto

--- forca.cpp
+++ forca.cpp
@@ -2,9 +2,21 @@
 #include<string>
 using namespace std;
 
+const string PALAVRA_SECRETA = "MELANCIA";
+
+//Declarando função
+bool letra_existe(char chute){
+    for(int i = 0; i < PALAVRA_SECRETA.size(); i++){
+        if(chute == PALAVRA_SECRETA[i]){
+            return true;
+        }
+    }
+    return false;
+}
+
+
 int main(){
-    string palavra_secreta = "MELANCIA";
-    cout<<palavra_secreta<< endl;
+    cout<<PALAVRA_SECRETA<< endl;
 
     bool nao_enforcou = true;
     bool nao_acertou = true;
@@ -13,7 +25,12 @@ int main(){
         char chute;
         cin>>chute;
 
-        cout<<"Seu chute foi: "<<chute<< endl;
+        if(letra_existe(chute)){
+            cout<<"Voce acertou. Seu chute esta na palavra."<< endl;
+        }
+        else{
+            cout<<"Voce errou. Seu chute nao esta na palavra."<< endl;
+        }
     }
 
 
